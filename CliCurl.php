392bd804e4134314cacfb3c171e45b57847bb81b
Alexey Karamyshev <voidakofficial@gmail.com>
2024-05-20 15:38:09 +0300

    add useragent and http method options

--- CliCurl.php
+++ CliCurl.php
@@ -2,16 +2,30 @@
 
 class CliCurl {
 
-    public function processWebRequest($url) {
+    public function processWebRequest($url, $userAgent='Mozilla/5.0', $httpMethod = 'GET') {
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $httpMethod);
  
         $webRequestResult = curl_exec($ch);
 
-        curl_close($ch);
+	if ($webRequestResult === false) {
+		throw new Exception(curl_error($ch));
+	}
+
+	$returnHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+	if ($returnHttpCode >= 400) {
+		throw new Exception("HTTP Request Failed with status code: {$returnHttpCode} !!!");
+	}
+
+	if (curl_close($ch) === false) {
+		throw new Exception("Error closing Curl session!");
+	}
 
         return $webRequestResult;
     }
--- pcurl.php
+++ pcurl.php
@@ -6,13 +6,14 @@ require_once __DIR__. DIRECTORY_SEPARATOR. 'autoload.php';
 $request = new CliCurl();
 
 $url = $argv[1];
+$useragent = $argv[2];
 
 if ($argc == 1) {
-    echo "\033[33mUsage: curl.php <domain>\033[0m". PHP_EOL;
+    echo "\033[33mUsage: curl.php <domain> <useragent> <method>\033[0m". PHP_EOL;
     exit(1);
 } else {
     try {
-        $result = $request->processWebRequest($url);
+        $result = $request->processWebRequest($url, $useragent);
         echo "\033[32m";
         var_dump($result);
         echo "\033[0m";
